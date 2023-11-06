42b9b5bc6bf438193a177f5a1a39e7460b0ef816
Sanchit Ahuja <t-sahuja@microsoft.com>
2023-11-06 08:14:24 +0000

    substrate bash script

---
+++ scripts/run_qa_substrate..sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Evaluating for MLQA"
+# for lang in ar de es hi vi zh en
+for lang in ar de es hi vi zh en
+do
+    echo "Running for language $lang"
+    python -m mega.eval_qa_gptturbo -p $lang -t $lang --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" -k 8 --substrate_prompt --model dev-gpt-35-turbo -e gpt4v2 --temperature 0 --num_evals_per_sec 2 -d mlqa
+done
