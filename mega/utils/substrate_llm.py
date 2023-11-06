046fa2cc01d38e064a7d26ce2712bd4ef9a62424
Sanchit Ahuja <t-sahuja@microsoft.com>
2023-11-06 11:17:42 +0000

    added validation check for palm languages

--- mega/utils/substrate_llm.py
+++ mega/utils/substrate_llm.py
@@ -13,7 +13,7 @@ load_dotenv("envs/melange.env")
 SUBSTRATE_API_KEY = os.environ["SUBSTRATE_API_KEY"]
 SUBSTRATE_API_AUTHORITY = os.environ["SUBSTRATE_API_AUTHORITY"]
 SUBSTRATE_UUID = os.environ["SUBSTRATE_UUID"]
-SUBSTRATE_GUID = os.environ.get("SUBSTRATE_GUID", '')
+SUBSTRATE_GUID = os.environ.get("SUBSTRATE_GUID", "")
 
 
 class LLMClient:
@@ -48,7 +48,7 @@ class LLMClient:
             "Accept": "application/json",
             "X-CV": f"{SUBSTRATE_UUID}-{str(uuid.uuid4())}",
         }
-        if SUBSTRATE_GUID != '':
+        if SUBSTRATE_GUID != "":
             headers["X-ScenarioGUID"] = SUBSTRATE_GUID
 
         body = str.encode(json.dumps(request))
@@ -154,13 +154,13 @@ if __name__ == "__main__":
     request_data = create_request_data(
         prompt=template,
         max_tokens=1000,
-        temperature=0,
+        temperature=1,
         top_p=1,
         n=1,
         stream=False,
         logprops=None,
         stop=None,
     )
-    completion = exec_llm(request_data, llm_client, model_name="dev-gpt-35-turbo")
-
-    print(f"{completion}")
+    for _ in range(5):
+        completion = exec_llm(request_data, llm_client, model_name="dev-gpt-35-turbo")
+        print(f"{completion}")
--- scripts/run_qa_gpt4.sh
+++ scripts/run_qa_gpt4.sh
@@ -2,7 +2,7 @@
 
 echo "Evaluating for MLQA"
 # for lang in ar de es hi vi zh en
-for lang in ar de es hi vi zh en
+for lang in es
 do
     echo "Running for language $lang"
     python -m mega.eval_qa_gptturbo -p $lang -t $lang --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" -k 8 --model palm -e gpt4v2 --temperature 0 --num_evals_per_sec 2 -d mlqa
