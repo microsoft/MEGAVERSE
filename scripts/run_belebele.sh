097c24a19cf3e887abef78bfbdb5330ae3741d17
t-daggarwal <t-daggarwal@microsoft.com>
2023-11-07 12:27:44 +0000

    add substrate prompt and fix xlsum

--- scripts/run_belebele.sh
+++ scripts/run_belebele.sh
@@ -39,6 +39,8 @@ do
 done
 
 
+
+# gpt 3.5 turbo experiments
 echo "Monolingual Evaluation"
 for prompt_name in "Choose the correct answer"
 do
@@ -50,14 +52,36 @@ do
             python -m mega.eval_belebele -p $lang -t $lang \
             --pivot_prompt_name "${prompt_name}" \
             --tgt_prompt_name "${prompt_name}" \
-            -k $k --model "gpt-35-turbo" \
+            -k $k --model "dev-gpt-35-turbo" \
             -e melange --temperature 0 \
             --log_wandb --timeout 30 \
-            --chat_prompt \
             -d "belebele" --substrate_prompt
         done
     done
 done
+
+
+# gpt 3.5 turbo experiments
+echo "Translate Test Evaluation"
+for prompt_name in "Choose the correct answer"
+do
+    for lang in english spanish german japanese french portuguese italian chinese_simplified dutch swedish turkish danish finnish russian norwegian korean chinese_traditional polish turkish hebrew arabic czech hungarian thai
+    do
+        for k in 0 
+        do
+            echo "Running for language $lang and prompt ${prompt_name} and k $k"
+            python -m mega.eval_belebele -p "en" -t $lang \
+            --pivot_prompt_name "${prompt_name}" \
+            --tgt_prompt_name "${prompt_name}" \
+            -k $k --model "dev-gpt-35-turbo" \
+            -e melange --temperature 0 \
+            --log_wandb --timeout 30 \
+            -d "belebele" --substrate_prompt \
+            --translate_test
+        done
+    done
+done
+
 
 
 echo "Monolingual Evaluation"
@@ -78,4 +102,28 @@ do
         done
     done
 done
+
+
+
+# gpt 3.5 turbo translate test experiments
+
+echo "Translate Test Evaluation"
+for prompt_name in "Choose the correct answer"
+do
+    for lang in english spanish german japanese french portuguese italian chinese_simplified dutch swedish turkish danish finnish russian norwegian korean chinese_traditional polish turkish hebrew arabic czech hungarian thai
+    do
+        for k in 0 
+        do
+            echo "Running for language $lang and prompt ${prompt_name} and k $k"
+            python -m mega.eval_belebele -p $lang -t $lang \
+            --pivot_prompt_name "${prompt_name}" \
+            --tgt_prompt_name "${prompt_name}" \
+            -k $k --model "meta-llama/Llama-2-7b-chat-hf" \
+            -e gpt4v3 --chat_prompt --temperature 0 \
+            --log_wandb --timeout 30 \
+            -d "belebele" --translate_test
+        done
+    done
+done
+
 
