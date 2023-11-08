99009681915de35ee1574e5d1af9096cfa66accd
Ashutosh Sathe <t-assathe@microsoft.com>
2023-11-08 06:28:41 +0000

    add a script for running risawoz + slight cleanup

--- mega/eval_xrisawoz.py
+++ mega/eval_xrisawoz.py
@@ -1,19 +1,22 @@
+import os
+import sys
 import json
 import pickle
 import random
-import sys
-import os
-from collections import defaultdict
 from tqdm import tqdm
-sys.path.append('/home/t-assathe/MEGA/MultilingualBlanketEval/')
+from dataclasses import dataclass
+from collections import defaultdict
+from simple_parsing import ArgumentParser
 from mega.models.completion_models import model_completion
-random.seed(1618)
-
-root_dir = '../xrisawoz_data/'
-num_learning_examples = 4
-model_name = 'palm' # name to be used in `model_completion`
-valid_fname = '0.1_valid.json' # remove 0.1 in case running on full dataset
-languages = ['en', 'hi', 'fr', 'ko', 'zh', 'enhi']
+# TODO: Unify chat and non-chat prompts
+@dataclass
+class XRiSAWOZArgs:
+    root_dir: str = '../xrisawoz_data/' # Path to data
+    num_learning_examples: int = 4 # Number of in-context examples
+    seed: int = 1618 # Seed for sampling random in-context examples
+    model_name: str = 'palm' # Name to be used in `model_completion`
+    valid_fname: str = '0.1_valid.json' # remove 0.1 in case running on full dataset
+    language: str = 'en' # Possible languages are ['en', 'hi', 'fr', 'ko', 'zh', 'enhi']
 
 def load_text(fname):
     with open(fname, 'r') as f:
@@ -37,7 +40,6 @@ prompts = {
     'da': 'prompt_da.txt',
     'rg': 'prompt_rg.txt',
 }
-prompts = {k: load_text(root_dir + v) for k, v in prompts.items()}
 task_to_out = {
     'dst': 'state',
     'api': 'api',
@@ -45,14 +47,21 @@ task_to_out = {
     'rg': 'response'
 }
 
-for language in languages:
-    out_fname = f'{root_dir}/{language}.{model_name.split("/")[-1]}.num_shots={num_learning_examples}.pkl'
+def main():
+    global prompts, task_to_out
+    parser = ArgumentParser()
+    parser.add_arguments(XRiSAWOZArgs, dest='xrisawoz')
+    args = parser.parse_args().xrisawoz
+    
+    random.seed(args.seed)
+    prompts = {k: load_text(args.root_dir + v) for k, v in prompts.items()}
+    out_fname = f'{args.root_dir}/{args.language}.{args.model_name.split("/")[-1]}.num_shots={args.num_learning_examples}.pkl'
     if not os.path.exists(out_fname):
         out = inf_ddict()
     else:
         with open(out_fname, 'rb') as f:
             out = pickle.load(f)
-    data = load_json(f'{root_dir}/processed/{language}/{valid_fname}')['data']
+    data = load_json(f'{args.root_dir}/processed/{args.language}/{args.valid_fname}')['data']
     inputs = defaultdict(lambda: defaultdict(list))
     for datum in data:
         inputs[datum['train_target']][datum['turn_id']].append(datum)
@@ -66,20 +75,21 @@ for language in languages:
                 for i, datum in enumerate(tqdm(inputs[task][turn_id])):
                     if not isinstance(out[datum['dial_id']]['turns'][turn_id][task_to_out[task]], defaultdict):
                         continue
-                    if m - 1 <= num_learning_examples:
+                    if m - 1 <= args.num_learning_examples:
                         correct_turn_id = turn_id
-                        while len(inputs[task][correct_turn_id]) - 1 <= num_learning_examples:
+                        while len(inputs[task][correct_turn_id]) - 1 <= args.num_learning_examples:
                             correct_turn_id -= 1
                     else:
                         correct_turn_id = turn_id
                     m_dash = len(inputs[task][correct_turn_id])
                     # sample examples other than this current one
                     possible_choices = [icl_i for icl_i in range(m_dash) if icl_i != i]
-                    icl_indices = random.sample(possible_choices, num_learning_examples)
+                    icl_indices = random.sample(possible_choices, args.num_learning_examples)
                     messages = [{
                         'role': 'system',
                         'content': prompts[task]
                     }]
+                    # TODO: Extract in context learning prompts somewhere nicer
                     for j, icl_i in enumerate(icl_indices):
                         icl_datum = inputs[task][correct_turn_id][icl_i]
                         messages.append({
@@ -94,8 +104,9 @@ for language in languages:
                         'role': 'user',
                         'content': f'Target Example\nTurn ID: "{datum["turn_id"]}"\nDatabase: "{datum["task"]}"\nContext: "{datum["input_text"]}"\nAnswer:'
                     })
+                    # TODO: Check if it's a chat model and use a chat prompt
                     final_prompt = '\n'.join(x['content'] for x in messages) + '\n'
-                    response = model_completion(final_prompt, model_name, lang=language[-2:], max_tokens=256)
+                    response = model_completion(final_prompt, args.model_name, lang=args.language[-2:], max_tokens=256)
                     print(datum['input_text'])
                     print(datum['output_text'])
                     print(response)
@@ -107,3 +118,6 @@ for language in languages:
             pickle.dump(out, f)
     with open(out_fname, 'wb') as f:
         pickle.dump(out, f)
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/run_xrisawoz.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+echo "Monolingual Evaluation"
+
+for model in dev-gpt-35-turbo;
+do
+    for lang in en hi fr ko zh enhi
+    do
+        python -m mega.eval_xrisawoz \
+            --root_dir "./xrisawoz_data/" \
+            --num_learning_examples 4 \
+            --seed 1618 \
+            --model_name "$model" \
+            --valid_fname "compressed_0.1_valid.json" \
+            --language "$lang" 
+    done
+done
