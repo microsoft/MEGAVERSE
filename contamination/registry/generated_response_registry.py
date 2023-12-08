69b5bf72852d5237df44fbdd60d84cb2200102b8
Sanchit Ahuja <t-sahuja@microsoft.com>
2023-12-08 10:55:19 +0000

    contamination done almost

--- contamination/registry/generated_response_registry.py
+++ contamination/registry/generated_response_registry.py
@@ -2,10 +2,12 @@ from contamination.utils.parse_generated_response_utils import (
     generate_xnli_str_from_generated_response,
     generate_xcopa_str_from_generated_response,
     generate_pawsx_str_from_generated_response,
+    generated_udpos_str_from_generated_response,
 )
 
 GENERATED_RESPONSE_REGISTRY = {
     "xnli": generate_xnli_str_from_generated_response,
     "xcopa": generate_xcopa_str_from_generated_response,
     "paws-x": generate_pawsx_str_from_generated_response,
+    "udpos" : generated_udpos_str_from_generated_response
 }
--- contamination/utils/parse_generated_response_utils.py
+++ contamination/utils/parse_generated_response_utils.py
@@ -71,3 +71,26 @@ def generate_pawsx_str_from_generated_response(
         generated_str += "Label: " + option["Label"].strip() + "\n"
         generated_str += "\n"
     return generated_str
+
+
+def generated_udpos_str_from_generated_response(
+    generated_response: List[Dict[str, str]]
+):
+    """Generate a string from the generated response.
+
+    Args:
+        generated_response (List[Dict[str,str]]): Generated response
+
+    Returns:
+        str: Generated string
+    """
+    generated_str = ""
+    for i, option in enumerate(generated_response):
+        generated_str += chr(ord("A") + i)
+        generated_str += ") "
+        generated_str += "tokens: " + option["tokens"].strip() + "\n"
+        generated_str += "tags: " + option["tags"].strip() + "\n"
+        generated_str += "tagged_tokens: " + option["tagged_tokens"].strip() + "\n"
+        generated_str += "\n"
+
+    return generated_str.strip()
