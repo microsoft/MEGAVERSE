9c0e30d295f8e4a66479483ce4e5a7ff886e92a8
t-daggarwal <t-daggarwal@microsoft.com>
2024-03-05 07:58:41 +0000

    fix afrqia

--- hf_scripts/run_afriqa.sh
+++ hf_scripts/run_afriqa.sh
@@ -18,13 +18,13 @@ save_dir=${arg:-"results"}
 
 echo $save_dir
 
-echo "Evaluating GPT-4 on AfriQA"
+echo "Evaluating Gemma on AfriQA"
 
 for model in "google/gemma-7b-it" "google/gemma-2b-it"
 do 
     for lang in fon hau ibo swa zul 
     do
         echo "Running for language $lang"
-        python -m mega.eval_afriqa_gptturbo -p $lang -t $lang --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" -k 8 --model $model -e melange --chat_prompt --temperature 0 --num_evals_per_sec 2 -d afriqa --test_frac 0.15 --from_hf_hub --save_dir $save_dir $save_dir
+        python -m mega.eval_afriqa_gptturbo -p $lang -t $lang --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" -k 8 --model $model -e melange --chat_prompt --temperature 0 --num_evals_per_sec 2 -d afriqa --test_frac 0.15 --from_hf_hub --save_dir $save_dir
     done
 done
