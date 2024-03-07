0d20eb6cc602899f165872dad3e97b46ebf6f09b
Ashutosh Sathe <t-assathe@microsoft.com>
2024-03-07 10:46:48 +0000

    Gemini related changes

--- scripts/run_xcopa_all.sh
+++ scripts/run_xcopa_all.sh
@@ -7,6 +7,6 @@ do
     do
         k=8
         echo "Running for language $lang and prompt ${prompt_name} and k $k"
-        python -m mega.eval_xcopa -p $lang -t $lang --pivot_prompt_name "${prompt_name}" --tgt_prompt_name "${prompt_name}" -k $k --model "dev-moonshot" -e gpt4v2 -d xcopa --timeout 30 --use-val-to-prompt --substrate_prompt --chat_prompt
+        python -m mega.eval_xcopa -p $lang -t $lang --pivot_prompt_name "${prompt_name}" --tgt_prompt_name "${prompt_name}" -k $k --model "gemini-pro" -d xcopa --timeout 30 --use-val-to-prompt
     done
 done
